7555295ccc6717e4d788742e0e4e0b5e7f250e8f
RudzaniMatidze <rdzmatidze@gmail.com>
2024-05-27 14:22:19 -0700

    file updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,12 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors that returns a JSON response"""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
